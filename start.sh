dbc5da7fe3387c3badfd4928e1ff0e1e02ca61e4
Владимир <svr93@i.ua>
2015-04-13 01:30:25 +0400

    add_frontend_data_receiving

--- start.sh
+++ start.sh
@@ -1,2 +1,15 @@
 iptables -I INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+
+cd applications/main/static
+
+rm README.md
+
+git init
+git remote add origin https://github.com/svr93/military_ed_client_prod
+git pull origin master
+
+cp -f main.html ../app/html.template
+
+cd ../../../
+
 node server.js
